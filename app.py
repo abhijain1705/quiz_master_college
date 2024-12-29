fa1f9a199dace30df8c5d3f183bd3aebaa89c389
abhijain <abhijain3002@gmail.com>
2024-12-29 11:00:06 +0530

    auth work

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask
 from models import db  # Import db from models/__init__.py
 from models.model import User
 from datetime import date
+from controllers.home import home as home_blueprint
+from controllers.auth import auth as auth_blueprint
+import uuid
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__, instance_relative_config=True)
+app.config['DEBUG'] = True
+app.secret_key = "merisecretkeyhai"
 
 #Path to database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
@@ -15,17 +21,16 @@ app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 
 # blueprint for auth routes in our app
-from controllers.auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+# blueprint for home routes in our app
+app.register_blueprint(home_blueprint)
 
 def init_db():    
+    # generate a random UUID
+    random_uuid = str(uuid.uuid4())
     # Create admin user
-    admin = User(id='1', username='admin@mail.com', password='admin1234', full_name='Admin', qualification='Btech', dob=date(2003,4,5), user_type='admin', created_at=date.today(), updated_at=date.today())
+    admin = User(id=random_uuid, username='admin@mail.com', password=generate_password_hash('admin@123', method='scrypt'), full_name='Admin', qualification='Btech', dob=date(2003,4,5), user_type='admin', created_at=date.today(), updated_at=date.today())
     db.session.add(admin)
     db.session.commit()
 
