78c5f2327fc237bf6077861101be2abf6234dcb6
abhijain <abhijain3002@gmail.com>
2024-12-29 22:36:15 +0530

    documentation writing

--- controllers/user.py
+++ controllers/user.py
@@ -1,5 +1,5 @@
 from models import db
-from models.model import User
+from models.model import User, Score, Quiz
 from datetime import datetime,date
 from config import admin_credentials
 from flask_login import login_required
@@ -69,6 +69,8 @@ user = Blueprint('user', __name__, url_prefix='/user')
                             'full_name': {'type': 'string'},
                             'qualification':{'type':'string'},
                             'dob':{'type':'string'},
+                            'score':{'type':'integer'},
+                            'quiz_played':{'type':'integer'},
                             'user_type' : {'type':'string'},
                             'created_at': {'type': 'string'},
                             'updated_at': {'type': 'string'}
@@ -88,6 +90,8 @@ user = Blueprint('user', __name__, url_prefix='/user')
                         'full_name': 'johndoe',
                         'qualification':'Btech',
                         'dob':'2021-01-01T00:00:00Z',
+                        'score':100,
+                        'quiz_played':1,
                         'user_type': 'admin',
                         'created_at': '2021-01-01T00:00:00Z',
                         'updated_at': '2021-01-01T00:00:00Z'
@@ -121,11 +125,21 @@ def getUsers():
         users = User.query.filter_by(**where).order_by(**order).limit(take).offset(skip).all()
         users_list = []
         for user in users:
+            score_of_user = Score.query.filter_by(user_id=user.id).all()
+            total_score = 0
+            quiz_played = 0
+            quize_of_user = Score.query.filter_by(user_id=user.id).all()
+            for quiz in quize_of_user:
+                quiz_played += 1
+            for score in score_of_user:
+                total_score += score.total_scored
             users_list.append({
                 'id': user.id,
                 'username': user.username,
                 'full_name': user.full_name,
                 'qualification': user.qualification,
+                'score': total_score,
+                'quiz_played': quiz_played,
                 'dob': user.dob,
                 'user_type': user.user_type,
                 'created_at': user.created_at,
