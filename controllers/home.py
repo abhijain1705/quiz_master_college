95d66a35717056405febfca280ae824fe65025f8
abhijain <abhijain3002@gmail.com>
2024-12-29 15:37:06 +0530

    bootstrap work

--- controllers/home.py
+++ controllers/home.py
@@ -1,5 +1,5 @@
 # controllers/home.py
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
 home = Blueprint('home', __name__, url_prefix='/')
@@ -7,4 +7,6 @@ home = Blueprint('home', __name__, url_prefix='/')
 @home.route("/")
 @login_required
 def home_route():
-    return "Welcome to the Home Page!"
+    if current_user.user_type == 'admin':
+        return render_template("admin.html")
+    return render_template("user.html")
