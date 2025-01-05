bc796135d2ed9c4ba3344c30b335cd95653c5de2
abhijain <abhijain3002@gmail.com>
2025-01-05 21:13:11 +0530

    wip

--- controllers/home.py
+++ controllers/home.py
@@ -11,3 +11,18 @@ def home_route():
         if current_user.user_type == 'admin' or session['user_type'] == 'admin':
             return render_template("admin.html")
         return render_template("user.html")
+
+
+@home.route("/admin_subject")
+@login_required
+def admin_subject():
+    if session['username'] or current_user:
+        if current_user.user_type == 'admin' or session['user_type']=='admin':
+            return render_template("admin_subject.html")
+
+@home.route("/admin_quiz")
+@login_required
+def admin_quiz():
+    if session['username'] or current_user:
+        if current_user.user_type == 'admin' or session['user_type']=='admin':
+            return render_template("admin_quiz.html")
