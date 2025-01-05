8c9afbf09e079ac2e228770ac5a79848694020d0
abhijain <abhijain3002@gmail.com>
2025-01-05 22:22:47 +0530

    wip

--- controllers/user.py
+++ controllers/user.py
@@ -4,13 +4,21 @@ from datetime import datetime,date
 from config import admin_credentials
 from flask_login import login_required
 from flasgger import Swagger, swag_from
+from controllers.decorator import role_required
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify
-
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
+@user.route("/")
+@login_required
+@role_required("user")
+def user_home():
+    return render_template("user.html")
+    
+
 @user.route("/get_all_users", methods=['POST'])
 @login_required
+@role_required("user")
 @swag_from({
     'tags':['User'],
     'summary':'Retrieve all users with filtering, pagination and sorting',
