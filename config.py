abd550c15c4f2ce246a14f404cf9dd9a3efb439f
abhijain <abhijain3002@gmail.com>
2025-01-18 14:19:38 +0530

    wip

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 # config.py
+# import math
+
+
+# def getPagesCount(total_rows, take):
+#     total_pages = math.ceil(total_rows/take)
+#     page_numbers = []
+#     max_page_cnt = 3
+
+#     # logic to determin the start and end number display
+
 admin_credentials = {
-    "username": "admin@mail.com",
+    "email": "admin@mail.com",
     "password": "admin@123"
 }
