228e6bac63c3bb0924d60c94ef92a9ec794b766a
abhijain <abhijain3002@gmail.com>
2025-01-18 20:29:35 +0530

    wip

--- models/model.py
+++ models/model.py
@@ -20,10 +20,9 @@ class Subject(db.Model):
     id=db.Column(db.VARCHAR(100), primary_key=True)
     name=db.Column(db.VARCHAR(100), nullable=False)
     description=db.Column(db.VARCHAR(100), nullable=False)
-    created_at  =db.Column(db.TIMESTAMP, nullable=False)
+    created_at=db.Column(db.TIMESTAMP, nullable=False)
     updated_at=db.Column(db.TIMESTAMP, nullable=False)
-    code = db.Column(db.VARCHAR(100), nullable=False)
-    credit = db.Column(db.Integer, nullable=False)
+    code=db.Column(db.VARCHAR(100), nullable=False)
 
 # score model
 class Score(db.Model):
