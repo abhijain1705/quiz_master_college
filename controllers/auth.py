4e2c26f7a564e100641e6a9f1dd09210975a1d61
abhijain <abhijain3002@gmail.com>
2025-01-18 14:21:15 +0530

    wip

--- controllers/auth.py
+++ controllers/auth.py
@@ -4,7 +4,7 @@ from models.model import User
 from datetime import datetime,date
 from config import admin_credentials
 from flasgger import Swagger, swag_from
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify
 
@@ -19,11 +19,11 @@ def login():
     'tags':['Auth'],
     'parameters':[
          {
-            'name': 'username',
+            'name': 'email',
             'in': 'formData',
             'type': 'string',
             'required': True,
-            'description': 'The username of the user'
+            'description': 'The email of the user'
         },
         {
             'name': 'password',
@@ -43,38 +43,32 @@ def login():
         400: {
             'description': 'Invalid input',
             'examples': {
-                'application/json': {'error': 'Username is required'}
+                'application/json': {'error': 'email is required'}
             }
         }
     }
 })
 def login_post():
-    username = request.form.get("email")
+    email = request.form.get("email")
     password = request.form.get("password")
-    session['username'] = username
-    if username==admin_credentials['username']:
+    session['email'] = email
+    if email==admin_credentials['email']:
         session['user_type'] = 'admin'
     else:
         session['user_type'] = 'user' 
 
-    # Validate required fields
-    if not username or not password:
+    if not email or not password:
         flash("All fields are required!", "danger")       
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
     
-    user = User.query.filter_by(username=username).first() # if this returns a user, then the email exists in database
+    user = User.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.', "danger")
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=True)
-    print("user ye hai", user)
-    # if the above check passes, then we know the user has the right credentials
     return redirect(url_for("admin.admin_home" if user.user_type =='admin' else 'user.user_home'))
 
     
@@ -140,52 +134,46 @@ def signup():
 })
 def signup_post():
     full_name=request.form.get("full_name")
-    username = request.form.get("email")
+    email = request.form.get("email")
     password = request.form.get("password")
     qualification=request.form.get("qualification")
     dob=request.form.get("dob")
 
-    # Validate required fields
-    if not full_name or not username or not password or not dob:
+    if not full_name or not email or not password or not dob:
         flash("All fields are required!", "danger")
         return redirect(url_for("auth.signup"))
 
-    user = User.query.filter_by(username=username).first() # if this returns a user, then the email exists in database
+    user = User.query.filter_by(email=email).first()
 
     if user:
         flash("Email already exists. Please use a different email.", "danger")
-        return redirect(url_for('auth.signup')) # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('auth.signup'))
     
 
-    # Convert dob to date object
     try:
         dob_date = datetime.strptime(dob, "%Y-%m-%d").date()
     except ValueError:
         flash("Invalid date format. Please use YYYY-MM-DD.", "danger")
         return redirect(url_for("auth.signup"))
 
-    # Hash the password
     hashed_password = generate_password_hash(password, method="scrypt")
 
-    # generate a random UUID
     random_uuid = str(uuid.uuid4())
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user=User(id=random_uuid,username=username, password=hashed_password, full_name=full_name,qualification=qualification, dob=dob_date, user_type='user', created_at=date.today(), updated_at=date.today())
+    new_user=User(id=random_uuid,email=email, password=hashed_password, full_name=full_name,qualification=qualification, dob=dob_date, user_type='user', created_at=date.today(), updated_at=date.today())
 
-    # Add the user to the database
     try:
         db.session.add(new_user)
         db.session.commit()
         flash("Registration successful!", "success")
         return redirect(url_for("auth.login"))
     except Exception as e:
-        print(e, "error hai bc")
         db.session.rollback()
         flash("An error occurred during registration. Please try again.", "danger")
         return redirect(url_for("auth.signup"))
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for("auth.login"))
         
