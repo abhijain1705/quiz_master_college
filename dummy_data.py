250a6eb40c6225b171e2917d85eb5de467051bfd
abhijain <abhijain3002@gmail.com>
2025-01-18 20:29:26 +0530

    wip

--- dummy_data.py
+++ dummy_data.py
@@ -1,8 +1,8 @@
 import uuid
-from models import db
-from models.model import Subject
-from datetime import date
 import random
+from models import db
+from datetime import datetime
+from models.model import Subject
 
 def create_dummy_subjects(count):
     for _ in range(count):
@@ -12,9 +12,8 @@ def create_dummy_subjects(count):
             name=f"Subject {_}",
             description=f"This is a description for Subject {_}",
             code=f"SUB{random.randint(100, 999)}",
-            credit=random.randint(1, 5),
-            created_at=date.today(),
-            updated_at=date.today()
+            created_at=datetime.now(),
+            updated_at=datetime.now()
         )
         db.session.add(subject)
     db.session.commit()
