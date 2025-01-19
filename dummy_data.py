be366c203df63659819aef8772c96fc5213f54be
abhijain <abhijain3002@gmail.com>
2025-01-19 17:50:00 +0530

    wip

--- dummy_data.py
+++ dummy_data.py
@@ -2,7 +2,26 @@ import uuid
 import random
 from models import db
 from datetime import datetime
-from models.model import Subject, Chapter
+from models.model import Subject, Chapter, Quiz
+
+def create_dummy_quiz(count):
+    for x in range(count):
+        random_uuid = str(uuid.uuid4())
+        quiz = Quiz(
+        id=random_uuid,
+        chapter_id='eedbc85d-67f1-4f1f-beb2-25abb142d736',
+        chapter_code='CHP181',
+        date_of_quiz=datetime.now(),
+        time_duration=3600000,
+        total_marks=100,
+        number_of_questions=random.randint(100, 999),
+        remarks=f"This is a remarks for Quiz {x}",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+        )
+        db.session.add(quiz)
+    db.session.commit()
+    print(f"{count} dummy quiz created successfully!")
 
 def create_dummy_chapters(count):
     for x in range(count):
@@ -11,7 +30,7 @@ def create_dummy_chapters(count):
         id=random_uuid,
         name=f"Chapter {x}",
         description=f"This is a description for Chapter {x}",
-        subject_id='ad867322-86c0-407e-a8f6-a4dcadeabcf4',
+        subject_id='9f3deac7-65ed-42b8-b07d-ad69aadafb4c',
         created_at=datetime.now(),
         pages=100,
         code=f"CHP{random.randint(100, 999)}",
