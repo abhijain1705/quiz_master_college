70944a54df88563ee7176d7f2c8a724d89b35bc7
abhijain <abhijain3002@gmail.com>
2025-01-19 16:32:07 +0530

    wip

--- controllers/home.py
+++ controllers/home.py
@@ -6,10 +6,9 @@ home = Blueprint("home", __name__, url_prefix="/")
 @home.route("/")
 @login_required
 def home_home():
-    # Check if the user is authenticated and their role matches
     if not current_user.is_authenticated:
         return redirect(url_for("auth.login"))
-    # Redirect unauthorized users to their respective home
+
     if current_user.user_type == "admin":
         return redirect(url_for("admin.admin_home"))
     return redirect(url_for("user.user_home")) 
