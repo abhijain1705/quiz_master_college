721d3e7027bf476e83e6ea769e59a87273794ee2
abhijain <abhijain3002@gmail.com>
2025-01-21 13:12:08 +0530

    wip status

--- dummy_data.py
+++ dummy_data.py
@@ -12,13 +12,14 @@ def create_dummy_questions(count):
         question_title=f'Dummy Question {x}',
         question_statement=f'Dummy Question Statement {x}',
         created_at=datetime.now(),
-        quiz_id='6abf10dd-8be0-411e-9f6f-63e0f9987708',
-        chapter_id='0339fa7e-8787-44c3-90be-7386ca24dd3a',
-        chapter_code='CHP711',
+        quiz_id='62723d7c-3a0b-4248-a65d-4e95bbd4b67d',
+        chapter_id='67d3f88b-18c0-4bef-b60a-3a306c236b51',
+        chapter_code='CHP772',
         option_1=f'Dummy Option 1 {x}',
         option_2=f'Dummy Option 2 {x}',
         option_3=f'Dummy Option 3 {x}',
         option_4=f'Dummy Option 4 {x}',
+        isActive=True,
         correct_option=random.randint(1, 4),
         marks=4,
         updated_at=datetime.now(),
@@ -27,18 +28,26 @@ def create_dummy_questions(count):
     db.session.commit()
     print("dummy questions created successfully")
 
+def delete_all_quiz():
+    all_quizzes = Quiz.query.all()
+    for quiz in all_quizzes:
+        db.session.delete(quiz)
+    db.session.commit()
+    print("dummy quizzes deleted successfully")
+
 def create_dummy_quiz(count):
     for x in range(count):
         random_uuid = str(uuid.uuid4())
         quiz = Quiz(
+        isActive=True,
         id=random_uuid,
         quiz_title=f'Quiz Title{x}',
-        chapter_id='0339fa7e-8787-44c3-90be-7386ca24dd3a',
-        chapter_code='CHP711',
+        chapter_id='344835b5-8c52-4ea3-8825-42e42e1d21f4',
+        chapter_code='CHP961',
         date_of_quiz=datetime.now(),
         time_duration=3600000,
         total_marks=100,
-        number_of_questions=random.randint(100, 999),
+        number_of_questions=random.randint(10, 25),
         remarks=f"This is a remarks for Quiz {x}",
         created_at=datetime.now(),
         updated_at=datetime.now(),
@@ -52,9 +61,10 @@ def create_dummy_chapters(count):
         random_uuid = str(uuid.uuid4())
         chapter = Chapter(
         id=random_uuid,
+        isActive=True,
         name=f"Chapter {x}",
         description=f"This is a description for Chapter {x}",
-        subject_id='f57d8803-5d28-4918-8f00-effc67a80c8d',
+        subject_id='2a4e5bf6-32f3-4552-88d0-b5d88f1af361',
         created_at=datetime.now(),
         pages=100,
         code=f"CHP{random.randint(100, 999)}",
@@ -71,6 +81,7 @@ def create_dummy_subjects(count):
         subject = Subject(
             id=random_uuid,
             name=f"Subject {_}",
+            isActive=True,
             description=f"This is a description for Subject {_}",
             code=f"SUB{random.randint(100, 999)}",
             created_at=datetime.now(),
