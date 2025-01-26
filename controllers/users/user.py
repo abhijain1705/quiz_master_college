621de3acf5a1cf2aaa05843c07c443bb8d22d0fd
abhijain <abhijain3002@gmail.com>
2025-01-26 16:37:19 +0530

    wip admin almost done

--- controllers/users/user.py
+++ controllers/users/user.py
@@ -1,10 +1,10 @@
 from models import db
-from models.model import User, Score, Quiz
+from flasgger import  swag_from
 from datetime import datetime,date
 from config import admin_credentials
 from flask_login import login_required
-from flasgger import  swag_from
 from controllers.decorator import role_required
+from models.model import User, Score, Quiz, Chapter, Subject
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, jsonify
 
 user = Blueprint('user', __name__, url_prefix='/user')
@@ -27,5 +27,11 @@ def user_scores():
 @login_required 
 @role_required("user")
 def user_quizzes():
-    return render_template("user/quizzes.html")
-        
+    quizzes = Quiz.query.filter(Quiz.date_of_quiz > date.today()).all()
+    if not quizzes:
+        return render_template("user/quizzes.html", quizzes=[], subject=None, chapter=None)
+
+    chapter = Chapter.query.filter_by(id=quizzes[0].chapter_id).first()
+    subject = Subject.query.filter_by(id=chapter.subject_id).first() if chapter else None
+
+    return render_template("user/quizzes.html", quizzes=quizzes, subject=subject, chapter=chapter)
